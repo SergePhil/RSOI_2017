2508472c7c9c27ed0982ad6748c7b17603763e98
Sergei Filatov <serj.phil@outlook.com>
2017-09-10 22:02:22 +0300

    localhost allowed

--- WebApp_One/settings.py
+++ WebApp_One/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '45q^1tqk1#(kagc(!-pqo#y1$7z_a_cf8#m91x7b4iny8h%3-$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rsoi-web-app.herokuapp.com',]
+ALLOWED_HOSTS = [
+    'rsoi-web-app.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
