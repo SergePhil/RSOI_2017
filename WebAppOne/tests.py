1044809410766fa526302b2afcd0918ff94c1818
Sergei Filatov <serj.phil@outlook.com>
2017-09-10 22:02:57 +0300

    More tests

--- WebAppOne/tests.py
+++ WebAppOne/tests.py
@@ -6,4 +6,16 @@ from django.test import TestCase
 class GreeterTest(TestCase):
     def test_not_found_on_empty(self):
         response = self.client.get('/greet/')
-        self.assertEquals(response.status_code, 404)
+        self.assertEquals(response.status_code, 400)
+
+    def test_not_found_on_wrong_gender(self):
+        response = self.client.get('/greet/?gender=x')
+        self.assertEquals(response.status_code, 400)
+
+    def test_not_found_on_nil_last_name(self):
+        response = self.client.get('/greet/?gender=f')
+        self.assertEquals(response.status_code, 400)
+
+    def test_not_found_on_empty_last_name(self):
+        response = self.client.get('/greet/?gender=m&last_name=')
+        self.assertEquals(response.status_code, 400)
