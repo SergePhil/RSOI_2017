e590294d1207a0a41930f4f682b709be5c502f95
Sergei Filatov <serj.phil@outlook.com>
2017-09-10 20:13:26 +0300

    Simple test

--- WebApp_One/WebAppOne/tests.py
+++ WebApp_One/WebAppOne/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class GreeterTest(TestCase):
+    def test_not_found_on_empty(self):
+        response = self.client.get('/greet/')
+        self.assertEquals(response.status_code, 404)
